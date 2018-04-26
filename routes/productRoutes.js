11b93cb7379f8349dc9e07cc27c1d96120675d40
Jennifer Tristan <31359254+jt6161@users.noreply.github.com>
2018-04-26 15:45:28 -0700

    updates

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -7,4 +7,11 @@ router.get('/', function(req, res, next) {
   knex('products').then(products => res.json(products))
 });
 
+router.post("/", function(req, res){
+  console.log(req.body);
+  knex("products").insert(req.body).then(()=>{
+    res.json({success:true})
+  })
+})
+
 module.exports = router;
