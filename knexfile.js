e9053fe7bf29a89ad226c2859b19f877d1503c78
Jennifer Tristan <Jennifer.Tristan@SerpicoDev.com>
2018-08-17 15:22:00 -0700

    fixed order of bath bombs

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,22 @@
-// let connectionString = process.platform === 'win32' ? 'postgres://postgres:root@localhost/420skincare' : 'postgres://localhost/420skincare'
-
 module.exports = {
   development: {
-      client: 'pg',
-      connection: 'postgres://localhost/420skincare',
-      migrations: {
-          directory: __dirname + '/db/migrations',
-        },
-      seeds: {
-          directory: __dirname + '/db/seeds',
-        },
+    client: 'pg',
+    connection: 'postgres://localhost/420skincare',
+    migrations: {
+      directory: __dirname + '/db/migrations'
     },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
   production: {
-      client: 'pg',
-      connection: process.env.DATABASE_URL,
-      migrations: {
-          directory: __dirname + '/db/migrations',
-        },
-      seeds: {
-          directory: __dirname + '/db/seeds',
-        },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
     },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  }
 };
