943d1f8fe14edeccf0ee12685e17ec9efc64aa9e
Jennifer Tristan <Jennifer.Tristan@SerpicoDev.com>
2018-11-16 01:16:14 -0700

    cleaning up code

--- routes/mediblesRoutes.js
+++ routes/mediblesRoutes.js
@@ -7,14 +7,4 @@ router.get('/', function (req, res, next) {
     knex('medibles').then(products => res.json(products))
 });
 
-//Don't need the route below since the user is not posting anything
-// router.post("/", function (req, res) {
-//     console.log(req.body);
-//     knex("medibles").insert(req.body).then(() => {
-//         res.json({
-//             success: true
-//         })
-//     })
-// })
-
 module.exports = router;
--- routes/thcproductRoutes.js
+++ routes/thcproductRoutes.js
@@ -7,11 +7,4 @@ router.get('/', function(req, res, next) {
   knex('thcproducts').then(products => res.json(products))
 });
 
-router.post("/", function(req, res){
-  console.log(req.body);
-  knex("thcproducts").insert(req.body).then(()=>{
-    res.json({success:true})
-  })
-})
-
 module.exports = router;
