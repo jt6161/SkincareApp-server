89b4ad604e062f35a0fea359982db4d5fec80a74
Jennifer Tristan <Jennifer.Tristan@SerpicoDev.com>
2018-11-28 16:11:33 -0700

    updated product page url

--- db/seeds/1_products.js
+++ db/seeds/1_products.js
@@ -16,7 +16,7 @@ exports.seed = function(knex, Promise) {
         img_url: 'https://static.wixstatic.com/media/7026a3_85050c20818b447ab3e5fc5b2a29fe2f~mv2_d_3456_5184_s_4_2.jpg',
         description: 'Scoop some 420 Skincare CBD Bath Bomb Dust in your tub and watch it fizz! Our bath salts are excellent, not only for relaxation, but also for skin health and detoxification. The following ingredients work with CBD for the ultimate bath bomb experience. Epson Salt, which absorbs through the skin to help flush toxins and help promote relaxation. White clay which assists in detoxification and purification of the skin and underlying issues. Sweet almond oil, which provides beneficial nutrients and vitamins. All bath bombs are hand-crafted and will, therefore, vary in appearance. Choose from 2 different scents! This product complies with all FDA standards and is not meant to diagnose, treat, cure or maintain any ailments or diseases in accordance with FDA guidelines.',
         price: '22.00',
-        url: 'https://www.420-skincare.com/product-page/cbd-bath-salts-lavender-1'
+        url: 'https://www.420-skincare.com/product-page/cbd-bath-bomb-dust'
         }, {
         product_id: '0550afac-6ed5-c3c3-cd1b-f80167a45739',
         name: 'CBD Body Cream - 4oz',
@@ -44,7 +44,7 @@ exports.seed = function(knex, Promise) {
         img_url: 'https://static.wixstatic.com/media/7026a3_3b4f791c968f49c49ae5fd522e2f7780~mv2_d_4032_3024_s_4_2.jpg',
         description: '420 Skincare 100% natural and organic CBD soap is perfectly suited for the sensitive skin. Our soap is deliciously aromatic and has medicinal properties provided by the cannabis plant, making it an excellent choice for people who live a healthy lifestyle and only want the best for their bodies. Choose your favorite scent! This product complies with all FDA standards and is not meant to diagnose, treat, cure or maintain any ailments or diseases in accordance with FDA guidelines.',
         price: '10.00',
-        url: 'https://www.420-skincare.com/product-page/cbd-soap-strawberry-mystery'
+        url: 'https://www.420-skincare.com/product-page/cbd-soap-blueberry-orange'
       }, {
         product_id: '87d878a5-afc3-6150-21b0-55e8851f3661',
         name: 'CBD Bath Bomb - Twin Pack',
