b8daf2f9ed81ade916ebf756b7217d6926ae121d
Jennifer Tristan <Jennifer.Tristan@SerpicoDev.com>
2019-02-10 21:30:08 -0700

    updates to images

--- db/seeds/7_medibles.js
+++ db/seeds/7_medibles.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       return knex('medibles').insert([
         {
             name: 'One Big Badass Brownie',
-            img_url: 'https://static.wixstatic.com/media/7026a3_1445f4039f7a4b8ca9fe5c33a05bd133~mv2.png',
+            img_url: 'https://static.wixstatic.com/media/7026a3_3984f426409942dd973589410ebdbdc1~mv2_d_5184_3456_s_4_2.jpg',
             description: '420 Medibles One Big Badass Brownie - 150 mg THC. This Brownie is a great big treat for anyone that loves brownies and edibles. This Treat is packed with 1.5 grams raw cannabis. Produced by MThomas LLC. Scottsdale Arizona. DO NOT USE IF YOU ARE NURSING OR PREGNANT. KEEP OUT OF REACH OF CHILDREN AND ANIMALS.'
         },
         {
@@ -16,17 +16,17 @@ exports.seed = function(knex, Promise) {
         },
         {
             name: 'One Big Badass Krispy Treat - Fruity Pebbles',
-            img_url: 'https://static.wixstatic.com/media/7026a3_1445f4039f7a4b8ca9fe5c33a05bd133~mv2.png',
+            img_url: 'https://static.wixstatic.com/media/7026a3_b5be883af9b94674b9728bc702a05791~mv2_d_5184_3456_s_4_2.jpg',
             description: '420 Medibles One Big Badass Krispy Treat Fruity Pebbles flavor - 150 mg THC. This Krispy Fruity Treat is a great big treat for anyone that loves edibles. This Treat is packed with 1.5 grams raw cannabis. Produced by MThomas LLC. Scottsdale Arizona. DO NOT USE IF YOU ARE NURSING OR PREGNANT. KEEP OUT OF REACH OF CHILDREN AND ANIMALS.'
         },
         {
             name: 'One Big Badass S’More - Original Campfire',
-            img_url: 'https://static.wixstatic.com/media/7026a3_1445f4039f7a4b8ca9fe5c33a05bd133~mv2.png',
+            img_url: 'https://static.wixstatic.com/media/7026a3_c8fe618bf67b448096f8168879d593e7~mv2_d_5184_3456_s_4_2.jpg',
             description: '420 Medibles One Big Badass S’More Original Campfire flavor - 150mg THC. Remember eating smores as a kid? Well this Big Badass S’More is the “grown-up” version. This Treat is packed with 1.5 grams raw cannabis. Produced by MThomas LLC. Scottsdale Arizona. DO NOT USE IF YOU ARE NURSING OR PREGNANT. KEEP OUT OF REACH OF CHILDREN AND ANIMALS.'
         },
         {
             name: 'One Big Badass Gummy - Sweet & Sour',
-            img_url: 'https://static.wixstatic.com/media/7026a3_1445f4039f7a4b8ca9fe5c33a05bd133~mv2.png',
+            img_url: 'https://static.wixstatic.com/media/7026a3_25f2a2d7b6fc4d8b954865ed67e7e1e4~mv2_d_5184_3456_s_4_2.jpg',
             description: '420 Medibles One Big Badass Gummy Sweet & Sour flavor. Everyone loves gummies, give this this big gummy a try! This Gummy is packed with 1.5 grams raw cannabis. Produced by MThomas LLC. Scottsdale Arizona. DO NOT USE IF YOU ARE NURSING OR PREGNANT. KEEP OUT OF REACH OF CHILDREN AND ANIMALS.'
         },
       ]);
