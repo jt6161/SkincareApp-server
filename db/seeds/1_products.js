4d29f2cab5687209acd6e3fcfa9890be55a235f6
Jennifer Tristan <Jennifer.Tristan@SerpicoDev.com>
2019-03-20 22:32:47 -0700

    updates to products added tincture and combo pack

--- db/seeds/1_products.js
+++ db/seeds/1_products.js
@@ -33,6 +33,18 @@ exports.seed = function(knex, Promise) {
         img_url: 'https://static.wixstatic.com/media/7026a3_1f33fc90df2f4db29549c3c7f9b11b56~mv2_d_5184_3456_s_4_2.jpg',
         description: 'Drop a 420 Skincare CBD bath bomb in your tub and watch it fizz! Our bath bombs twin packs are excellent, not only for relaxation, but also for skin health and detoxification. The following ingredients work with CBD for the ultimate bath bomb experience. Epson Salt, which absorbs through the skin to help flush toxins and help promote relaxation. White clay which assists in detoxification and purification of the skin and underlying issues. Sweet almond oil, which provides beneficial nutrients and vitamins. All bath bombs are hand-crafted and will, therefore, vary in appearance. Choose your favorie scents. This product complies with all FDA standards and is not meant to diagnose, treat, cure or maintain any ailments or diseases in accordance with FDA guidelines.',
         price: '22.99'
+      },
+      {
+        name: 'CBD Tincture',
+        img_url: 'https://static.wixstatic.com/media/7026a3_a6307231615e43709b2882ec7381f780~mv2_d_3024_4032_s_4_2.jpeg',
+        description: 'CBD Tincture naturally-prepared with Black Seed Oil and can provide a variety of potentially beneficial cannabinoids. 500mg 1oz This product complies with all FDA standards and is not meant to diagnose, treat, cure or maintain any ailments or diseases in accordance with FDA guidelines.',
+        price: '79.99'
+      },
+      {
+        name: 'CBD Bath Bomb & Body Butter Combo',
+        img_url: 'https://static.wixstatic.com/media/7026a3_bcc78442548c47ee8bbd7c3fcc357c11~mv2_d_4956_4956_s_4_2.jpg',
+        description: 'Drop a 420 Skincare CBD bath bomb in your tub and watch it fizz! Our bath bombs twin packs are excellent, not only for relaxation, but also for skin health and detoxification. The following ingredients work with CBD for the ultimate bath bomb experience. Epson Salt, which absorbs through the skin to help flush toxins and help promote relaxation. White clay which assists in detoxification and purification of the skin and underlying issues. Sweet almond oil, which provides beneficial nutrients and vitamins. All bath bombs are hand-crafted and will, therefore, vary in appearance. Choose your favorie scents.With a thicker consistency than body cream, 420 SkinCares CBD body butter is a Skin Super Food that is crafted from seeds found in the Shea tree fruit. Naturally rich in vitamins, our CBD body butter provides the skin with essential fatty acids and nutrients necessary for the collagen production. 420 SkinCare CBD body butter also provides relief from sore joints, rashes circulation, sunburns, arthritis, chronic pain, psoriasis and lupus. This product complies with all FDA standards and is not meant to diagnose, treat, cure or maintain any ailments or diseases in accordance with FDA guidelines.',
+        price: '65.98'
       }
     ]);
   });
